111052f44ed0efcb45622ee2d3f64bc56b56c300
joachimprinzbach <joachim.prinzbach@gmail.com>
2019-11-16 08:37:15 +0100

    Initial commit

---
+++ src/main/kotlin/com/prinzbach/kloudassistant/KloudAssistantApplication.kt
@@ -0,0 +1,11 @@
+package com.prinzbach.kloudassistant
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KloudAssistantApplication
+
+fun main(args: Array<String>) {
+    runApplication<KloudAssistantApplication>(*args)
+}
---
+++ src/test/kotlin/com/prinzbach/kloudassistant/KloudAssistantApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.prinzbach.kloudassistant
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KloudAssistantApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
